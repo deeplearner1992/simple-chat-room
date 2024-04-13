a21db8bd7e36d93d0ca9a1727e0aeaad745a5a88
cmwong <cmwong831@gmail.com>
2024-04-13 15:30:30 +0800

    removed redunant codes

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -12,9 +12,6 @@ const store = useIsLoggedInStore()
 <template>
   <fwb-heading tag="h2" class="text-center mb-5" v-if="!store.isLoggedIn">&#128513; WELCOME TO CHAT CHAT &#128540;</fwb-heading>
   <header class="container justify-center center">
-    <!-- <div>
-      <h1 v-if="!store.isLoggedIn">WELCOME TO CHAT CHAT</h1>
-    </div> -->
     <div>
       <LoginView v-if="!store.isLoggedIn"></LoginView>
       <ChatRoomView v-else></ChatRoomView>
@@ -42,105 +39,4 @@ const store = useIsLoggedInStore()
   padding: 10px;
   border-radius: 25px;
 }
-
-/* header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-.container {
-  display: flex;
-  flex-direction: column;
-}
-
-
-.justify-center {
-  justify-content: center;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-  
-}
-
-@media (max-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-  
-} */
 </style>

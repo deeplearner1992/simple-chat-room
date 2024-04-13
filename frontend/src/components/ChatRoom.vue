4ca747bd42b2fafdcda610d5b55e8f620caaed22
cmwong <cmwong831@gmail.com>
2024-04-13 21:47:51 +0800

    removed redundant code

--- frontend/src/components/ChatRoom.vue
+++ frontend/src/components/ChatRoom.vue
@@ -143,11 +143,6 @@ function addMountain() {
           </fwb-dropdown>
         </div>
       </div>
-      <div class="flex flex-row">
-  <div class="basis-1/4">01</div>
-  <div class="basis-1/4">02</div>
-  <div class="basis-1/2">03</div>
-</div>
     </div>
   </header>
 

5893e53b6995f864e9f40de6e74006ec3f9812bc
cmwong <cmwong831@gmail.com>
2024-04-13 15:29:49 +0800

    removed redunant codes

--- frontend/src/components/ChatRoom.vue
+++ frontend/src/components/ChatRoom.vue
@@ -151,53 +151,6 @@ function addMountain() {
 </template>
 
 <style scoped>
-/* header {
-  line-height: 1.5;
-  max-height: 100vh;
-} */
-
-/* .logo {
-  display: block;
-  margin: 0 auto 2rem;
-} */
-
-/* nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-} */
-
-/* nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-} */
-
-/* nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-} */
-
-/* nav a:first-of-type {
-  border: 0;
-} */
-
-/* .chatbox {
-  overflow: auto;
-}
-
-.message-container {
-  display: flex;
-  flex-direction: column;
-} */
-
-/* .container {
-  flex-direction: column;
-} */
 .chat-container {
   /* max-width: 600px; */
   width: 80%;
@@ -279,36 +232,4 @@ nav a.router-link-exact-active:hover {
   justify-content: center;
   text-align: center;
 }
-
-/* @media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-
-  .justify-center {
-    justify-content: center;
-    text-align: center;
-  }
-} */
 </style>
--- frontend/src/components/Login.vue
+++ frontend/src/components/Login.vue
@@ -45,33 +45,11 @@ function register(e: any) {
 </script>
 
 <template>
-  <!-- <div>
-    <h1>LOGIN</h1>
-    <form @submit.prevent="login">
-      <input v-model="username" placeholder="username" />
-      <br />
-      <br />
-      <input v-model="password" placeholder="password" type="password" />
-      <br />
-      <br />
-      <button type="submit">Login</button>
-    </form>
-  </div> -->
-  <!-- <fwb-button @click="showModal"> Open modal </fwb-button> -->
-
-  <!-- <fwb-modal v-if="isShowModal" @close="closeModal"> -->
   <fwb-heading tag="h4" class="text-center">Login</fwb-heading>
   <fwb-input v-model="username" label="Username" placeholder="enter your username" size="lg" />
   <fwb-input v-model="password" label="Password" placeholder="enter your password" size="lg" />
   <div class="p-2 flex">
-    <!-- <div class="w-1/2">Empty Space</div> -->
     <div class="flex justify-between w-full">
-      <!-- <button type="submit" class="bg-gray-500 text-white p-2 rounded text-sm w-auto">
-        Cancel
-      </button>
-      <button type="submit" class="bg-yellow-500 text-white p-2 ml-6 rounded text-lg w-auto">
-        Save
-      </button> -->
       <Register />
       <fwb-button @click="register" color="green"> Login </fwb-button>
     </div>
